362b6caf4a3626101baf178f7fd1802b4c5c8c04
Arnaud Panapadeatchy <arnaud.panapadeatchy@dsinstruments.fr>
2022-10-14 16:07:26 +0400

    Project Structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-  return src('*.scss')
+  return src('citadel/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['citadel/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
